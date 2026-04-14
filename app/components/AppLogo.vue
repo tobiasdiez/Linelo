fcab5bcb734d86a06c4401d7e33420c16d8c12fa
Tobias Diez <code@tobiasdiez.de>
2026-04-14 21:22:16 +0200

    feat: add logo component

---
+++ app/components/AppLogo.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+const DEFAULT_LOGO_SIZE = 96
+
+const {
+  alt = 'Linelo',
+  size = DEFAULT_LOGO_SIZE,
+} = defineProps<{
+  alt?: string
+  size?: number | string
+}>()
+</script>
+
+<template>
+  <img
+    :src="'/logo.svg'"
+    :alt="alt"
+    :style="{ width: typeof size === 'number' ? `${size}px` : size }"
+    class="app-logo"
+  >
+</template>
+
+<style scoped>
+.app-logo {
+  display: block;
+  height: auto;
+}
+</style>
