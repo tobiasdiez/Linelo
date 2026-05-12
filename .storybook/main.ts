4aa6ee92950bc60be856cb85ee1f90f4942cdb62
Tobias Diez <code@tobiasdiez.de>
2026-05-12 22:20:39 +0200

    chore: add ci and other basic github config

---
+++ .storybook/main.ts
@@ -0,0 +1,16 @@
+import type { StorybookConfig } from '@nuxtjs/storybook'
+
+const config: StorybookConfig = {
+  // Need to specify stories as workaround for https://github.com/storybookjs/storybook/issues/20761
+  stories: ['../components/*.stories.vue'],
+  core: {
+    // @ts-expect-error - need to update storybook types
+    disableTelemetry: true,
+  },
+  addons: ['storybook-vue-addon', '@chromatic-com/storybook'],
+  framework: {
+    name: '@storybook-vue/nuxt',
+    options: {},
+  },
+}
+export default config
