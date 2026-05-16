a991a781e58bf7fc0818f04d0402fb7b5688737a
Tobias Diez <code@tobiasdiez.de>
2026-05-16 14:57:43 +0200

    chore: add color palette (#1)

---
+++ app/app.config.ts
@@ -0,0 +1,13 @@
+export default defineAppConfig({
+  ui: {
+    colors: {
+      primary: 'primary',
+      secondary: 'secondary',
+      success: 'primary',
+      info: 'neutral',
+      warning: 'amber',
+      error: 'red',
+      neutral: 'neutral',
+    },
+  },
+})
