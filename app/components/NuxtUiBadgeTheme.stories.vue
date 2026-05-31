eb3fcfa6fe43505c8249ba9027ccf7286266c075
Tobias Diez <code@tobiasdiez.de>
2026-05-31 22:20:26 +0200

    chore: update Nuxt UI config and add component stories (#13)
    * chore: update Nuxt UI config and add component stories
    
    * fix linter
    
    * fix stories and nuxt ui config
    
    * fix linter
    
    * fix linter

---
+++ app/components/NuxtUiBadgeTheme.stories.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { UBadge } from '#components'
+</script>
+
+<template>
+  <Stories title="Nuxt UI/Badge Theme">
+    <Story title="Default">
+      <UBadge>Default primary soft</UBadge>
+    </Story>
+    <Story title="Secondary">
+      <UBadge color="secondary">Secondary soft</UBadge>
+    </Story>
+    <Story title="Neutral">
+      <UBadge color="neutral">Neutral soft</UBadge>
+    </Story>
+  </Stories>
+</template>
---
+++ app/components/NuxtUiCardTheme.stories.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { UCard } from '#components'
+</script>
+
+<template>
+  <Stories title="Nuxt UI/Card Theme">
+    <Story title="Default">
+      <UCard>
+        <p class="text-base text-toned">Default card.</p>
+      </UCard>
+    </Story>
+
+    <Story title="Soft Inset">
+      <UCard variant="soft">
+        <p class="text-base text-toned">Soft card variant.</p>
+      </UCard>
+    </Story>
+  </Stories>
+</template>
